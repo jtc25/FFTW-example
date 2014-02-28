6d09fef887e3ab9ec1be5d855d91134f0e4526a7
Jim <jim@Jims-iMac.local>
2014-02-28 10:54:24 -0500

    Initial Commit

---
+++ FFTW example/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  FFTW example
+//
+//  Created by Jim on 2/28/14.
+//  Copyright (c) 2014 Jim. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
